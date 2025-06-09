9d8f2619dc6077ecb0c91fb638df8a78e347ff89
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-06-09 13:37:08 +0000

    feat: add loginForm page

---
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -0,0 +1,34 @@
+export default function LoginForm() {
+    return(
+        <form>
+      <div className="mb-4">
+        <label className="block text-sm font-medium mb-2" htmlFor="email">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="w-full p-2 border border-gray-300 rounded"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium mb-2" htmlFor="password">
+          Senha
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="w-full p-2 border border-gray-300 rounded"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-[#D8671E] text-white py-2 rounded hover:bg-[#C0581A] transition duration-200"
+      >
+        Entrar
+      </button>
+    </form>
+    )
+}
